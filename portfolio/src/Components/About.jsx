8de5b695b012987756346f330695e07570afc4ca
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-12-09 17:49:23 +0300

    Added some features in the About section

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -1,13 +1,98 @@
-import React from 'react'
-import "./about.css"
+import React, { useEffect, useState } from 'react';
+import './about.css';
+import { FaReact, FaDatabase, FaHtml5, FaCss3, FaJs } from 'react-icons/fa';
+import { DiRuby } from 'react-icons/di';
 
 function About({ isDarkMode }) {
+  const [showSlideInText, setShowSlideInText] = useState(false);
+
+  useEffect(() => {
+    setShowSlideInText(true);
+  }, []);
+
+  // Sample skills and languages data
+  const frontendSkills = ['Responsive Web Design', 'Web Frameworks', 'HTML5', 'CSS3', 'JavaScript'];
+  const backendSkills = ['Authentication and Authorization', 'RESTful API Design', 'Database Management'];
+  const otherSkills = ['Git', 'UI/UX Design', 'Problem Solving and Debugging', 'Punctual', 'Teamwork Coorperative'];
+
+  const languages = [
+    { name: 'HTML5', icon: <FaHtml5 />, bgColor: '#e34c26' },
+    { name: 'JavaScript', icon: <FaJs />, bgColor: '#f0db4f' },
+    { name: 'CSS3', icon: <FaCss3 />, bgColor: '#1572b6' },
+    { name: 'React.js', icon: <FaReact />, bgColor: '#61dafb' },
+    { name: 'Ruby', icon: <DiRuby />, bgColor: '#cc0000' },
+    { name: 'Ruby on Rails', icon: <DiRuby />, bgColor: '#cc0000' },
+    { name: 'SQLITE3', icon: <FaDatabase />, bgColor: '#336791' },
+    { name: 'POSTGRES', icon: <FaDatabase />, bgColor: '#336791' },
+  ];
+
   return (
     <div className={`about-page ${isDarkMode ? 'dark' : 'light'}`}>
-      <h2>About me</h2>
-      <h1>In this i will tell you more about me</h1>
+      {showSlideInText && (
+        <div className={`slide-in-text ${isDarkMode ? 'dark' : 'light'}`}>
+          <p>Hello there,</p>
+          <p>Thank you for considering to know more about me</p>
+          <p>Let's get started</p>
+        </div>
+      )}
+      <div className="about-content">
+        <p>
+          Welcome to my world! I'm a passionate web developer with a knack for
+          creating stunning and user-friendly websites. My journey in the world
+          of coding began several years ago, and since then, I've been on a
+          constant quest for knowledge and improvement.
+        </p>
+
+        {/* Skills Section */}
+        <div className="skills-section">
+          <h2>Skills</h2>
+          <div className="skill-categories">
+            <div className="skill-category">
+              <h3 className='skill-header'>Frontend</h3>
+              <ul>
+                {frontendSkills.map((skill, index) => (
+                  <li key={index}>{skill}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="skill-category-divider"></div>
+            <div className="skill-category">
+              <h3 className='skill-header'>Backend</h3>
+              <ul>
+                {backendSkills.map((skill, index) => (
+                  <li key={index}>{skill}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="skill-category-divider"></div>
+            <div className="skill-category">
+              <h3 className='skill-header'>Other</h3>
+              <ul>
+                {otherSkills.map((skill, index) => (
+                  <li key={index}>{skill}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {/* End of Skills Section */}
+
+        {/* Languages Section */}
+        <div className="languages-section">
+          <h2>Languages</h2>
+          <ul>
+            {languages.map((language, index) => (
+              <li key={index} style={{ backgroundColor: language.bgColor }}>
+                {language.icon}
+                {language.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* End of Languages Section */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -1,27 +1,131 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import bg from '../assets/portfolio-image.png';
 import about from '../assets/about-me.png';
-import "./home.css"
+import reviewsimg from "../assets/reviews.png"
+import './home.css';
+import Review from './Review';
+import avatar1 from '../assets/portfolio-avatar1.jpg';
+import avatar2 from '../assets/portfolio-avatar2.webp';
+import avatar3 from "../assets/MC OGA 1.jpg"
 
 function Home() {
+  const reviews = [
+    {
+      id: 1,
+      avatar: avatar1,
+      name: 'John M',
+      review: 'I had a great experience working with Patience. The website turned out amazing!',
+    },
+    {
+      id: 2,
+      avatar: avatar2,
+      name: 'Mary Kelley',
+      review: 'From functionality, to creativity, Patience created for me a website that not only stood out but attracted more customers!',
+    },
+    {
+      id: 3,
+      avatar: avatar3,
+      name: 'Mc OGA',
+      review: 'Even though mine was UI/UX design and not the full website, Patience created for me such an amazing design for my website!',
+    },
+    {
+      id: 4,
+      avatar: avatar2,
+      name: ' Jake',
+      review: 'I may not be a client but working with Patience was such a beautiful experience.She showed dedication and determination which lack in many teammates.',
+    },
+  ];
+
+  const [visibleReviews, setVisibleReviews] = useState(0);
+
+  const handleNextClick = () => {
+    setVisibleReviews((prev) => (prev + 1) % reviews.length);
+  };
+
+  const handlePrevClick = () => {
+    setVisibleReviews((prev) => (prev - 1 + reviews.length) % reviews.length);
+  };
+
+  const handleReviewsClick = () => {
+    // Scroll to or focus on the reviews section
+    const reviewsSection = document.getElementById('reviews');
+    if (reviewsSection) {
+      reviewsSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const canShowNext = reviews.length > visibleReviews + 3;
+  const canShowPrev = visibleReviews > 0;
+
+  // Typing effect
+  const introText = "A Web Developer";
+  const [typedText, setTypedText] = useState('');
+  const [isTypingComplete, setIsTypingComplete] = useState(false);
+
+  useEffect(() => {
+    let index = 0;
+    const intervalId = setInterval(() => {
+      setTypedText((prev) => introText.slice(0, index + 1));
+      index += 1;
+      if (index === introText.length) {
+        clearInterval(intervalId);
+        setTimeout(() => {
+          setIsTypingComplete(true);
+        }, 500)
+      }
+    }, 150);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello there,</span>
         <span className="introText">
-          I'm<span className="introName"> Patience</span> <br />A Web Developer
+          I'm<span className="introName"> Patience</span> <br />
+          {typedText}{isTypingComplete && <span className='blinking-cursor'>|</span>}
         </span>
         <p className="introPara">I'm a sophisticated Website Developer ready to meet all your website desire needs.</p>
-         <p className='introPara'>Want to learn more about me, click on the About me button below .</p>
+        <p className="introPara2">Want to learn more about me, click on the About me button below or click on the client reviews button
+         to see reviews from different clients .</p>
+        <div className='button-container'>
         <Link to="/about">
           <button className="btn">
-            <img src={about} alt="about" className='about-img' />
+            <img src={about} alt="about" className="about-img" />
             About me
           </button>
         </Link>
+        <Link to="#reviews">
+        <button className='btn' onClick={handleReviewsClick}>
+          <img src={reviewsimg} alt='reviews' className='about-img' />
+          <span>Client Reviews</span>
+        </button>
+        </Link>
+        </div>
       </div>
       <img src={bg} alt="Profile" className="bg" />
+
+      {/* Client review section */}
+      <section id="reviews" className="reviews">
+        <h2>Client Reviews</h2>
+        <div className="review-container">
+          {reviews.slice(visibleReviews, visibleReviews + 3).map((review) => (
+            <Review key={review.id} review={review} />
+          ))}
+        </div>
+        {canShowPrev && (
+          <button className="review-nav-btn" onClick={handlePrevClick}>
+            {'<'}
+          </button>
+        )}
+        {canShowNext && (
+          <button className="review-nav-btn" onClick={handleNextClick}>
+            {'>'}
+          </button>
+        )}
+      </section>
     </section>
   );
 }
